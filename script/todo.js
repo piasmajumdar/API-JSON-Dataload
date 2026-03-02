f2d10b0de50387294e77d993102a4d174c618b1e
piasmajumdar <piasmajumdar@gmail.com>
2026-03-02 18:49:49 +0530

    fetch todo list, show to UI, added terenary function to checkbox

--- script/todo.js
+++ script/todo.js
@@ -1,8 +1,8 @@
 const loadTodo = () => {
     const url = "https://jsonplaceholder.typicode.com/todos";
     fetch(url)
-    .then((res) => res.json())
-    .then((data) => displayTodo(data))
+        .then((res) => res.json())
+        .then((data) => displayTodo(data))
 }
 
 // {
@@ -23,11 +23,13 @@ const displayTodo = (todos) => {
         const todoCard = document.createElement('div');
         todoCard.innerHTML = `
         <div class = "todo-card">
-            <p>${todo.completed == true ?  `<i class="fa-solid fa-square-check"></i>`: `<i class="fa-regular fa-square-check"></i>`}</p>
+            <p>${todo.completed == true
+                ? `<i class="fa-solid fa-square-check"></i>`
+                : `<i class="fa-regular fa-square-check"></i>`}</p>
             <h4>${todo.title}</h4>
         </div>
-        `   
-    todoContainer.append(todoCard);
+        `
+        todoContainer.append(todoCard);
     });
 }
 loadTodo()
